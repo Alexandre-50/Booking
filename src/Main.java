1642c16de2f54a479dde645323a634c96244f44b
Alexandre Celles <alexandre.celles@edu.ece.fr>
2025-04-17 22:09:47 +0200

    Nouveau Test

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,8 @@ import java.awt.*;
 
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SwingUtilities.invokeLater(() -> {
             DaoFactory daoFactory = DaoFactory.getInstance("booking", "root", "");
             UtilisateurDAO utilisateurDAO = daoFactory.getUtilisateurDAO();
